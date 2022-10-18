ec03ce49e756b815d76cc2daadff26fb6b469e40
glebe <glebe@BELG04>
2022-10-19 00:30:35 +0200

    Добавьте файлы проекта.

---
+++ lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Лукьянов Глеб 22-ИСП-2\1 Уровень:Базовый Вариант: 2
+using System.Globalization;
+
+Console.Write("Ведите число А: ");
+     int a = int.Parse(Console.ReadLine());
+Console.Write("Ведите числа B: ");
+     int b = int.Parse(Console.ReadLine());
+
+for  (int i = b - 1; i > a; i--)
+{
+    Console.WriteLine(i);
+    
+}
+int p=b - a - 1;
+Console.WriteLine($"Количество чисел в промежутке {p}");  
